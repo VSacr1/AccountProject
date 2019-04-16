d8cd5754108d300f33aff57990940d30d9ffcb3b
Vsacre1 <vjsacre@gmail.com>
2019-04-16 21:59:10 +0100

    Add test 1

--- account/src/test/java/account/account/Testing.java
+++ account/src/test/java/account/account/Testing.java
@@ -1,5 +1,33 @@
 package account.account;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class Testing {
+
+	Map<Integer, Account> map = new HashMap<Integer, Account>();
+	AccountMap creatingmap = new AccountMap();
+	
+	@Before
+	public void beforeTesting()
+	{
+		
+		map.put(1, new Account("John", "Smith", 1038347));
+    	map.put(2, new Account("Max", "Smith", 3847659));
+    	map.put(3, new Account("Jessie", "Coal", 2947593));
+	}
+	
+	@Test
+	public void test1()
+	{
+		assertEquals("Name does not exist!!", "John", map.get(1).getFirstName());
+	}
+	
+
 
 }
